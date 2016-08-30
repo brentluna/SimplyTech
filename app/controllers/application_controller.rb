4a3b47c8aa78b90467ed4a329aeabc7895d54087
530farm <brentadamluna@gmail.com>
2016-08-30 08:38:02 -0700

    added webpackconfig, npm modules, frontend folders, and rendering staticpage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,28 +7,28 @@ class ApplicationController < ActionController::Base
 
 private
 
-def current_user
-  return nil unless session[:session_token]
-  @current_user ||= User.find_by(session_token: session[:session_token])
-end
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
 
-def logged_in?
-  !!current_user
-end
+  def logged_in?
+    !!current_user
+  end
 
-def login(user)
-  user.reset_session_token!
-  session[:session_token] = user.session_token
-  @current_user = user
-end
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
 
-def logout
-  current_user.reset_session_token!
-  session[:session_token] = nil
-  @current_user = nil
-end
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
 
-def require_logged_in
-  render json: {base: ['invalid credentials']}, status: 401 if !current_user
-
+  def require_logged_in
+    render json: {base: ['invalid credentials']}, status: 401 if !current_user
+  end
 end

bc6ddc9e97d82af0dae38305a2d85a570b98c066
530farm <brentadamluna@gmail.com>
2016-08-31 16:46:48 -0700

    added some formatting to feeds index

--- frontend/components/feeds/feed_index_item.jsx
+++ frontend/components/feeds/feed_index_item.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const FeedIndexItem = ({image, title, summary}) => (
-  <li>
-    <h3>{title}</h3>
-    <p>{summary}</p>
-    <img src={image} />
+  <li className='feed-index-item'>
+  <div className='img-div'>
+    <img className='feed-index-item-image' src={image} />
+  </div>
+    <div className='index-text-container'>
+    <h3 className='feed-index-item-title'>{title}</h3>
+      <p className='feed-index-item-summary'>{summary}</p>
+    </div>
   </li>
 );
 

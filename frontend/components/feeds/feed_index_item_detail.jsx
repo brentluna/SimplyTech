ca7a528bf86cb3c21097de191c6facfeffdfbf21
530farm <brentadamluna@gmail.com>
2016-09-01 08:47:05 -0700

    refactored css into more specific files, updated some ReaderModal elements

--- frontend/components/feeds/feed_index_item_detail.jsx
+++ frontend/components/feeds/feed_index_item_detail.jsx
@@ -8,10 +8,11 @@ const FeedIndexItemDetail = ({title, image, content, author, feed, published, ur
 
     <h1 className='reader-modal-title'>{title}</h1>
     <p className='reader-modal-content'>{content}</p>
+    <a href={url} target='_blank'>Read More...</a>
+
     <footer className='popout-footer'>
       <p>Author: {author}</p>
       <p>{published}</p>
-      <a href={url}>{feed}</a>
 
     </footer>
   </div>

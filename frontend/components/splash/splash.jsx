df2a9a6b995f4cebb5ebf9d0c127824681448ec2
530farm <brentadamluna@gmail.com>
2016-09-29 09:09:22 -0700

    updated splash page w/ description bar

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -17,25 +17,69 @@ class Splash extends React.Component {
       <div className='splash-header-container'>
 
           <h1 className='splash-header'>Welcome to simply<span>T</span>ech</h1>
-            <p className='splash-text'>Your one stop source for everything Tech</p>
+            <p className='splash-text'>Your one stop source for tech news!</p>
           {button}
         </div>
         {/* <div className='splash-image'> */}
+
         <ul className='splash-ul'>
+          <li className='splash-li'>
+            <ul className='splash-feature-ul'>
+              <li>
+                <div className='feature-div'>
+                  <i className="material-icons feature">rss_feed</i>
+                  <div className='icon-title-div'>
+                  <h4>Your blogs</h4>
+                  <p> 
+                    Dive deeper by following blogs from the latest movers, shakers and thinkers in the tech world. 
+                  </p>
+                  </div>
+                </div>
+              </li>      
+              <li>
+                <div className='feature-div'>
+                  <i className="material-icons feature">view_list</i>
+                  <div className='icon-title-div'>
+                  <h4>Read</h4>
+                  <p> 
+                    simplyTech offers a clean, minimalist reading experience optimized for productivity.
+                  </p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className='feature-div'>
+                  <i className="material-icons feature">apps</i>
+                  <div className='icon-title-div'>
+                  <h4>Organize</h4>
+                  <p> 
+                    Let the web work for you by arranging the content you rely on inot easy-to-read collections.
+                  </p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className='feature-div'>
+                  <i className="material-icons feature">bookmark_border</i>
+                  <div className='icon-title-div'>
+                  <h4>Save for later</h4>
+                  <p> 
+                    Save stories and easily get back to them at a later time.
+                  </p>
+                  </div>
+                </div>
+              </li>
+            </ul> 
+          </li>
           <li className='splash-li'>
             <img src='/assets/sidebar.png'/>
           </li>
-          <li className='splash-li'>
-            <img src='/assets/category.png'/>
-          </li>
+          
           <li className='splash-li'>
             <img src='/assets/SimplyTech.png'/>
           </li>
           <li className='splash-li'>
             <img src='/assets/add_feeds.png'/>
-          </li>
-          <li className='splash-li'>
-            <img src='/assets/add_to_collection.png'/>
           </li>
         </ul>
         {/* </div> */}
